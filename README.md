1a84e038d5dc3dbe4dae16a4d79651baccbe46b1
Eric Zhang <zhang.lyuan@gmail.com>
2015-09-01 14:29:09 +0800

    initial project, add the origin idea and draft code here

---
+++ README.md
@@ -0,0 +1,52 @@
+
+## How does this tool to generate the fan-in/out information
+### Step 1. Generate the call graph dot files via `Doxygen`
+We would like to generate the call graph dot files, we have to change the `Doxygen` configuration file as below:
+```
+EXTRACT_ALL            = YES
+INPUT                  = init kernel boot mm fs lib tools # where the source code will be parsed
+INLINE_SOURCES         = YES
+GENERATE_LATEX         = NO
+INCLUDE_PATH           = include                          # header file path
+HAVE_DOT               = YES
+CALL_GRAPH             = YES
+CALLER_GRAPH           = YES
+DOTFILE_DIRS           = dotfile
+DOT_CLEANUP            = ON
+```
+After execute the command `doxygen`, there will be generate the source code's index files and related inmediate files `*.dot`.
+
+### Step 2. Reading the `*.dot` file and analyze the call relationship between functions.
+The `*_cgraph.dot` file's format likes this:
+```
+digraph "function_name"
+{
+  edge [the fone setting of edge];
+  node [the font setting of node];
+  rakedir="LR";
+  Node1 [label="funtion_name", node's style setting]; //this is discribe itsself
+  Node1 -> Node2 [node font setting of the edge, to illustrate the relationship between Node1 and Node2]
+  Node2 [label="function_name", ..., URL="$filename_8c.html#hashcode"];
+  Node2 -> Node3 [node font setting of the edge, to illustrate the relationship between Node2 and Node3]
+  Node3 [label="function_name", ..., URL="$filename_8c.html#hashcode"];
+  ...
+}
+
+```
+tool `dot` generates the png pictures according to the `*.dot` file's definition. It's also give the clue to us for parsing the relationship between functions. If we know the relationship between function, we also create the relathionship between files/folders etc.
+
+We program intent on generate a relationships, and output as specific format, e.g. `*.json`
+
+**NOTE:** We shall carefully consider the template situation during parse C plus plus programming language source code.
+
+### Step 3 Visulize the functions relationship
+
+For such relationship data, we can store the Neo4j such graph database, and the database offer the simple chart to illustrate the data element's relationship.
+
+But we recommend use d3js to implement customized chart for these relationship visualization.
+
+
+
+
+
+
---
+++ maps.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+from os import listdir
+from os.path import isfile, join
+import re
+import json
+
+path = "/Users/erizhang/workspace/linux/html";
+#path = "./";
+
+
+def get_hash(f):
+    words = f.split("_");
+    return words[-2];
+
+def get_source_name(f):
+    words = ['_' if x =='' else x for x in f.split('_')];
+
+    extention = words[-3];
+    s = ['.' if x == '8' else x for x in list(extention)];
+    extention = "".join(s);
+    words[-3] = extention;
+
+    del words[-2:];
+
+    return "".join(words);
+
+# { name: free_inode;
+#   hash: aaa83f2a89dae6d03dfe15deae29f383a;
+#   file: tty_ioctrl.c;
+# }
+def get_function_name(lines):
+    func_name = lines[0].split(" ");
+    return func_name[1][1:-2];
+    
+def get_function_direct_calls(lines):
+    targets_nodes = [];
+    target_node = {};
+    target = "";
+    for line in lines:
+        words = line.lstrip().split(" ");
+        
+        if words[0] == "Node1" and words[1] == "->":
+            target = words[2];
+        if words[0] == target and words[1] != "->":
+            m = re.search(r'.*label=\"(.*)\",height.*\$(.*)\.html\#(.*)\"', line);
+            if (m == None):
+                continue;
+            target_node["name"] = m.group(1);
+            target_node["file"] = m.group(2);
+            target_node["hash"] = m.group(3);
+            targets_nodes.append(target_node);
+            
+
+    return targets_nodes;
+
+def make_unique(original_list):
+    unique_list = [];
+    map(lambda x: unique_list.append(x) if (x not in unique_list) else False, original_list)
+    return unique_list
+
+if __name__ == "__main__":
+    dotfiles = [f for f in listdir(path) if (isfile(join(path, f))and f.endswith(".dot"))];
+
+    cgraphfiles = [f for f in dotfiles if "_cgraph" in f];
+    nodes = [];
+    links = [];
+
+    for f in cgraphfiles:
+        node = {};
+        node["file"] = get_source_name(f);
+        node["hash"] = get_hash(f);
+
+        with open(join(path, f)) as fp:
+            lines = fp.readlines();
+            node["name"] = get_function_name(lines);
+            
+            targets = get_function_direct_calls(lines);
+        fp.close();
+
+        for t in targets:
+            link = {};
+            link["source"] = node["hash"];
+            link["target"] = t["hash"];
+            links.append(link);
+
+        nodes.append(node);
+        nodes = nodes + targets;
+
+    nodes = make_unique(nodes);
+    links = make_unique(links);
+
+    print json.JSONEncoder().encode({"nodes": nodes, "links": links});
+
