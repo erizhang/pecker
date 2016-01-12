e69570a6adbdd293779ba68e4381e53340551e62
erizhang <zhang.lyuan@gmail.com>
2016-01-12 13:40:59 +0800

    statistics the ccn and fan-out

--- call_tree.py
+++ call_tree.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 from os import listdir
 from os.path import isfile, join
 import re
@@ -7,7 +6,7 @@ import json
 
 import argparse
 
-path = "/home/erizhang/workspace/lab/sample/html"
+
 
 def get_hash(f):
     words = f.split("_");
@@ -15,9 +14,7 @@ def get_hash(f):
 
 def get_source_name(f):
     words = ['_' if x =='' else x for x in f.split('_')];
-    print words
     extention = words[-3];
-    print extention
     s = ['.' if x == '8' else x for x in list(extention)];
     extention = "".join(s);
     words[-3] = extention;
@@ -25,13 +22,8 @@ def get_source_name(f):
     del words[-2:];
 
     name = "".join(words);
-    print name
     return "".join(words);
 
-# { name: free_inode;
-#   hash: aaa83f2a89dae6d03dfe15deae29f383a;
-#   file: tty_ioctrl.c;
-# }
 def get_function_name(lines):
     func_name = lines[0].split(" ");
     return func_name[1][1:-2];
@@ -50,10 +42,9 @@ def get_function_direct_calls(lines):
             if (m == None):
                 continue;
             target_node["name"] = m.group(1);
-            target_node["file"] = m.group(2);
+            target_node["file"] = m.group(2).replace("_8", ".");
             target_node["hash"] = m.group(3);
             targets_nodes.append(target_node);
-            
 
     return targets_nodes;
 
@@ -62,18 +53,11 @@ def make_unique(original_list):
     map(lambda x: unique_list.append(x) if (x not in unique_list) else False, original_list)
     return unique_list
 
-if __name__ == "__main__":
-    
-#    parser = argparse.ArgumentParser(description='Process the input path')
-#    parser.add_argument('path', metavar='P', type=str, nargs='+', help='the path of dot files.')
-#    args = parser.parse_args()
-
+def read_call_tree(path):
     dotfiles = [f for f in listdir(path) if (isfile(join(path, f))and f.endswith(".dot"))];
-
     cgraphfiles = [f for f in dotfiles if "_cgraph" in f];
     nodes = [];
     links = [];
-    print cgraphfiles
 
     for f in cgraphfiles:
         node = {};
@@ -83,46 +67,52 @@ if __name__ == "__main__":
         with open(join(path, f)) as fp:
             lines = fp.readlines();
             node["name"] = get_function_name(lines);
-            targets = []
-            targets = get_function_direct_calls(lines);
-            node["fan-in"] = len(targets);
+            node['calls'] = get_function_direct_calls(lines);
         fp.close();
 
         nodes.append(node);
-        #nodes = nodes + targets;
-
-    icgraphfiles = [f for f in dotfiles if "_icgraph" in f];
-    print "------------------------"
-    print icgraphfiles
-    print "========================"
-    for f in icgraphfiles:
-        filename = get_source_name(f);
-        hashcode = get_hash(f);
-        fan_out = 0;
-
-        with open(join(path, f)) as fp:
-            lines = fp.readlines();
-            targets = [];
-            targets = get_function_direct_calls(lines);
-            fan_out = len(targets);
-            function_name = get_function_name(lines);
-        fp.close();
-
-        isFound = False;
-        for node in nodes:
-            if node["hash"] == hashcode:
-                node["fan-out"] = fan_out;
-                isFound = True;
-
-        if isFound == False:
-            new_node = {};
-            new_node['file'] = filename
-            new_node['hash'] = hashcode
-            new_node['name'] = function_name
-            new_node['fan-in'] = 0
-            new_node['fan-out'] = fan_out
-            nodes.append(new_node);
-            
     nodes = make_unique(nodes);
+    return nodes
 
+def read_complexity_list(complexity_file):
+    nodes = []
+    with open(complexity_file) as fp:
+        lines = fp.readlines();
+        del lines[0:3]
+        del lines[-4:]
+        for line in lines:
+            node = {};
+            keys = [key for key in line.split(' ') if key]
+
+            m = re.search(r'(.*)\@.*\@.*\/(.*)', keys[-1]);
+            if (m == None):
+                continue;
+            node['name'] = m.group(1);
+            node['file'] = m.group(2);
+            node['nloc'] = keys[0];
+            node['ccn'] = keys[1];
+
+            nodes.append(node);
+    fp.close();
+    nodes = make_unique(nodes);
+    return nodes
+
+
+def calc_fan_out(node, call_tree):
+    for n in call_tree:
+        if node['file'] == n['file'] and node['name'] == n['name']:
+            return len(n['calls']);
+    return 0
+
+def calc_fan_in(node, call_tree):
+    return 0
+
+if __name__ == '__main__':
+    call_tree_nodes = read_call_tree("/home/erizhang/workspace/lab/html");
+    nodes = read_complexity_list("./ctest");
+    for node in nodes:
+        node['fan-out'] = calc_fan_out(node, call_tree_nodes);
+        node['fan-in'] = calc_fan_in(node, call_tree_nodes);
+
+    print nodes
     print json.JSONEncoder().encode({"nodes": nodes});
