dec6957df837556687124bb4cbdd6e34cc28bf5e
erizhang <zhang.lyuan@gmail.com>
2016-04-15 16:07:02 +0800

    generize the walkinput.py

--- src/walkinput.py
+++ src/walkinput.py
@@ -2,19 +2,34 @@
 import os
 import os.path
 
+from src import shell
+
 source_dirs = []
 
-def isContainSourceFile(files):
+def isContainHeaderFile(files):
     for file in files:
         if file.endswith(".h") or file.endswith(".hpp"):
             return True
     return False
 
-def sources(dir):
-    for root, dirs, files in os.walk(dir):
-        if isContainSourceFile(files):
-            source_dirs.append(root)
+def isContainSrcFile(files):
+    for file in files:
+        if file.endswith(".cpp") or file.endswith(".cxx"):
+            return True
+    return False
 
-if __name__ == '__main__':
-    sources('/home/erizhang/cougar')
-    print " ".join(source_dirs)
+def walking(dir, is_header):
+    for root, dirs, files in os.walk(dir):
+        if is_header and isContainHeaderFile(files):
+            dirs_list.append(root)
+        if !is_header and isContainSrcFile(files):
+            dirs_list.append(root)
+        
+
+def list_header_files(config):
+    walking(config['code_base'], True)
+    return " ".join(dirs_list)
+
+def list_cpp_files(config):
+    walking(config['code_base'], False)
+    return " ".join(dirs_list)
