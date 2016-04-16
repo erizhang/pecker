cb7c5383a2b376bf7f61496cb1849cfee00fcc55
Eric Zhang <zhang.lyuan@gmail.com>
2016-04-17 00:17:16 +0800

    fix unit test case name with preffix, and add unit test running file

---
+++ runut.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../'))
+
+import unittest
+from test import test_doxygen_config
+
+if __name__ == '__main__':
+    SeTestSuite = unittest.defaultTestLoader.discover(start_dir='./')
+    unittest.TextTestRunner(verbosity=2).run(unittest.TestSuite(SeTestSuite))
--- test/test_doxygen_config.py
+++ test/test_doxygen_config.py
@@ -2,17 +2,13 @@ import unittest
 
 from doxygen import config
 
-class TestDoxygen(unittest.TestCase):
-    def config_update(self):
+class TestDoxygenConfig(unittest.TestCase):
+    def test_config_update(self):
         self.assertTrue(True)
 
-    def given_path_with_slash_when_convert_shall_with_backward_slash(self):
+    def test_given_path_with_slash_when_convert_shall_with_backward_slash(self):
         path_value = '/home/name/workspace/pecker /home/name'
         formated = config.format_path_expr(path_value)
         self.assertEqual('\/home\/name\/workspace\/pecker \/home\/name', formated)
-
-
-
-
-
+        
         
